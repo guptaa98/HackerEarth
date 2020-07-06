80e9fa21fa1e1768dd528fd545a00aa8c5737240
Prashasti Gupta <prashasti708@gmail.com>
2020-07-06 15:01:23 +0530

    Create Roy and Profile Picture.cpp

---
+++ Roy and Profile Picture.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int n,l,w,h;
+	cin>>l;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>w>>h;
+		if(w<l || h<l)
+		{
+			cout<<"UPLOAD ANOTHER"<<endl;
+		}
+		else if(w==h)
+		{
+			cout<<"ACCEPTED"<<endl;
+		}
+		else
+		{
+			cout<<"CROP IT"<<endl;
+		}
+	}
+}
